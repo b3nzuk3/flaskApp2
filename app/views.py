bbd175cd6030fac9b6a27e60b05926f5de64b7cd
b3nzuk3 <hussenito7@gmail.com>
2022-05-03 02:49:23 +0300

    added routes and statements in my view function

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from flask import render_template
+from app import app
+from .request import get_news
+
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    # Getting tesla new
+    tesla_news = get_news('tesla')
+    apple_news = get_news('apple')
+    print(tesla_news)
+    title = 'Home - Welcome to the best Movie Review Website online'
+    return render_template('index.html',title =title, tesla = tesla_news, apple = apple_news)
