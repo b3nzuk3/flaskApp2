e6cdda2ecfe1ffd3aeb9c7f3b673e83b061779fd
b3nzuk3 <hussenito7@gmail.com>
2022-05-03 02:05:58 +0300

    created my news class

---
+++ models/news.py
@@ -0,0 +1,11 @@
+class News:
+    """
+    News class
+    """
+    def __init__(self, author, description, url, urlToImage, publishedAt, content):
+        self.author =author
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
